25a244bb19aa9c133f3412aef39fb2b2f7450377
Addamsovka <reginabaron1221@gmail.com>
2020-07-11 16:59:25 +1000

    testing

--- RoomToFloorONE_script.py
+++ RoomToFloorONE_script.py
@@ -7,19 +7,13 @@ __author__ = 'Addamsovka'
 import sys
 import os
 
-from Autodesk.Revit.UI import TaskDialog
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter
 from Autodesk.Revit.DB import Element, XYZ, CurveArray
 from Autodesk.Revit.DB import Transaction, ElementId, Transform, Curve, CurveArray
 from Autodesk.Revit.DB import SpatialElementBoundaryOptions, Options, SpatialElement
-from Autodesk.Revit.DB.Architecture import RoomFilter, Room
-from Autodesk.Revit.Creation import *
-from System.Collections.Generic import List
-from System import ArgumentOutOfRangeException
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
-
 
 def main():
     floor_dict = {}  # {'name':'id'} Make floor type dictionary
@@ -30,63 +24,25 @@ def main():
     room_boundary_options = SpatialElementBoundaryOptions()  # SpatialElement class
 
     room_id = 269067
-    print(room_id)
     room = doc.GetElement(ElementId(room_id))
-
-    room_level_id = room.Level.Id
-    room_id = room.Id
-    room_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
-    print(room_name)
+    # room_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
     room_boundary = room.GetBoundarySegments(room_boundary_options)[0]
+    room_level_id = room.Level.Id
+    room_floor_finish = room.get_Parameter(BuiltInParameter.ROOM_FINISH_FLOOR).AsString()
+    if room_floor_finish not in floor_dict:
+        t = Transaction(doc, 'duplicate')
+        t.Start()
+        floor_type = doc.GetElement(floor_dict.get('Undefined'))
+        new_floor_type = floor_type.Duplicate(room_floor_finish)
+        add_floor_type(floor_dict, new_floor_type)
+        t.Commit()
     if len(room_boundary) > 0:
-        room_floor_finish = room.get_Parameter(BuiltInParameter.ROOM_FINISH_FLOOR).AsString()
-        if room_floor_finish not in floor_dict:
-            t = Transaction(doc, 'duplicate')
-            t.Start()
-            floor_type = doc.GetElement(floor_dict.get('Undefined'))
-            new_floor_type = floor_type.Duplicate(room_floor_finish)
-            add_floor_type(floor_dict, new_floor_type)
-            t.Commit()
         type_id = floor_dict.get(room_floor_finish)
-        new_floor = Floor(type_id, room_boundary, room_level_id, room_id)
+        new_floor = Floor(type_id, room_boundary, room_level_id)
         new_floor.make_floor()
         print(room_floor_finish + ' floor placed in ' + room_name + '.')
     else:
         print("Room boundary of " + room_name + " does not found.")
-
-
-# Help transaction wrapper
-def revit_transaction(transaction_name):
-    def wrap(f):
-        @wraps(f)
-        def wrapped_f(*args, **kwargs):
-            try:
-                t = Transaction(doc, transaction_name)
-                t.Start()
-            except InvalidOperationException as errmsg:
-                print('Transaciton Error: {}'.format(errmsg))
-                return_value = f(*args, **kwargs)
-            else:
-                return_value = f(*args, **kwargs)
-                t.Commit()
-            return return_value
-
-        return wrapped_f
-
-    return wrap
-
-
-# Select all floor types from Active project
-def select_all_floor_types():
-    global doc
-    return FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors).WhereElementIsElementType()
-
-
-# Add new floor type into existing dictionary, needs existing dictionary and a new floor type
-def add_floor_type(floor_dict, new_floor_type):
-    floor_dict[Element.Name.GetValue(new_floor_type)] = new_floor_type.Id
-    return floor_dict
-
 
 
 class Floor:
@@ -94,41 +50,50 @@ class Floor:
     Class floor for creating new floors from type id, boundary and level
 
     """
-    def __init__(self, type_id, boundary, level_id, room_id):
+    def __init__(self, type_id, boundary, level_id):
         self.type_id = type_id
         self.boundary = boundary
         self.level_id = level_id
 
-    # TODO solve issue here * curves - when room does not have a boundary
+    def boundary_to_array(self, boundary):
+        floor_curves = CurveArray()
+        if boundary[0].GetCurve().Z != 0:
+            for boundy in self.boundary:
+                curve = boundy.GetCurve()
+                a = curve.GetEndPoint(0)
+                myVector = XYZ(0, 0, (-a.Z))
+                tf = Transform.CreateTranslation(myVector)
+                curve = curve.CreateTransformed(tf)
+                floor_curves.append(curve)
+        else:
+            for boundary_segment in self.boundary:
+                floor_curves.Append(boundary_segment.GetCurve())
+        return floor_curves
+
     def make_floor(self):
         global doc
         t = Transaction(doc, 'Floor Creator')
         t.Start()
-        floor_curves = CurveArray()
-        # todo solve array vs boundary object control
-        for boundary_segment in self.boundary:
-            floor_curves.Append(boundary_segment.GetCurve())
-        if floor_curves[0].GetEndPoint(0).Z != 0:
-            print('test')
-            floor_curves = transform_boundary_lines(floor_curves)
+        floor_curves = self.boundary_to_array(self.boundary) # how is it with self?
         floor_type = doc.GetElement(self.type_id)
         level = doc.GetElement(self.level_id)
         normal = XYZ.BasisZ
         doc.Create.NewFloor(floor_curves, floor_type, level, False, normal)
         t.Commit()
 
-def transform_boundary_lines(room_boundary):
-    point = room_boundary.GetEndPoint(0)
-    myVector = XYZ(0,0,(-point.Z))
-    tf = Transform.CreateTranslation(myVector)
-    room_boundary = room_boundary.CreateTransformed(tf)
-    return room_boundary
+# Select all floor types from Active project
+def select_all_floor_types():
+    global doc
+    return FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors).WhereElementIsElementType()
 
-# Code:
+# Add new floor type into existing dictionary, needs existing dictionary and a new floor type
+def add_floor_type(floor_dict, new_floor_type):
+    floor_dict[Element.Name.GetValue(new_floor_type)] = new_floor_type.Id
+    return floor_dict
+
 main()
 
-
-
+# testing floors / these worked until last one
 # 134997
 # Undefined floor placed in Chodba.
 # 135009
