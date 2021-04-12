2d4aafd1a3e34d34874397202adc4aa6ac4ca366
Addamsovka <64003254+Addamsovka@users.noreply.github.com>
2021-04-12 19:02:23 +1000

    Update AddSharedAndProjectParameters.py

--- AddSharedAndProjectParameters.py
+++ AddSharedAndProjectParameters.py
@@ -49,9 +49,10 @@ Preferebly use with node Data.ImportExcel. One parameter in a row.
   String, # parName (exapmle: Name-DE)
   String, # parGroupName (example: My New Group)
   String, # parType (exapmle: Text)
-  String, # parCaterories separeted by "," (exapmle: Generic Models,Casework,Furniture) - no spaces after ,
+  String, # parCaterories separeted by "," (exapmle: Generic Models, Casework, Furniture)
   String, # instance (for type parameter set: false; for instance parameter set: true)
   String, # parDescription (description of parameter)
+  //TODO Group Parameter Under - in next version
   ]
 """
 params = IN[0]
