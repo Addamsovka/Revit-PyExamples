79c808ce5be06b2118ee4633bd22641d8543aac0
Addamsovka <64003254+Addamsovka@users.noreply.github.com>
2021-04-12 18:21:19 +1000

    Update AddSharedAndProjectParameters.py

--- AddSharedAndProjectParameters.py
+++ AddSharedAndProjectParameters.py
@@ -1,6 +1,6 @@
 # Version Dynamo Core 2.3.0.5885
 # Version Dynamo Revit 2.3.0.7661
-# Create Shared Parameters from Excel and bind them to categories as Project Parameters
+# Create Shared Parameters from string-lists (Excel) and bind them to categories as Project Parameters
 # Made by Addamsovka 2021
 # Version 1.0
 
@@ -42,18 +42,31 @@ app = uiapp.Application
 uidoc = uiapp.ActiveUIDocument
 
 # Inputs from user
+"""
+Format of excel file needs to be in exact structure or next lines needs to be adjusted.
+[
+  String, # parName (exapmle: Name-DE)
+  String, # parGroupName (example: My New Group)
+  String, # parType (exapmle: Text)
+  String, # parVisible (true)
+  String, # parCaterories separeted by "," (exapmle: Generic Models, Casework, Furniture)
+  String, # parGroup (example: Text)
+  String, # instance (for type parameter set: false; for instance parameter set: true)
+  String, # parDescription (description of parameter)
+  ]
+"""
 params = IN[0]
 out = []
 
-for param in params:  # All data are strings - will be converted accordigly
-    parName = param[0]  # Gets String for Parameter Name
-    parGroupName = param[1]  # Gets String for add SharedParameter into a group
-    parType = param[2]  # String used for getting ParameterType from list
-    parVisible = param[3]  # Will be converted to bool
-    parCaterories = param[4]  # Will be converted to CategorySet
-    parGroup = param[5]  # Gets String
-    instance = param[6]  # Will be converted to bool
-    parDescription = param[7]  # Gets String
+for param in params:
+    parName = param[0]  # Parameter Name
+    parGroupName = param[1]  # To which group add the SharedParameter
+    parType = param[2]  # ParameterType needs to be from existing list in Revit
+    parVisible = param[3]  # Will be converted to bool/ TODO not needed in this version
+    parCaterories = param[4]  # Main Categories from Revit
+    parGroup = param[5]  # Gets String / Not needed in this version
+    instance = param[6]  # Is instance? If not then Type
+    parDescription = param[7]  # Description od parameter
     out.append("Adding new parameter..." + parName)
     
     try:
@@ -64,7 +77,7 @@ for param in params:  # All data are strings - will be converted accordigly
         groups = sharedParameterFile.Groups
 
         if not groups.Item[parGroupName]:
-            groups.Create(parGroupName)  # Creates group if dose not exist
+            groups.Create(parGroupName)  # Creates group if those not exist
 
         # Get all Definitions of parameters in group
         externalDefinitions = groups.Item[parGroupName].Definitions
